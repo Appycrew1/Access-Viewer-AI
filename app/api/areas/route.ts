59d1cdd54a3b06106615fa82665744e8c7e9173b
Appycrew1 <appycrew1@gmail.com>
2025-09-01 22:25:15 +0100

    Delete app/api directory

--- app/api/areas/route.ts
+++
@@ -1,4 +0,0 @@
-import { NextResponse } from 'next/server'
-import { SAMPLE } from '@/lib/sample'
-export const dynamic='force-dynamic'
-export async function GET(){ return NextResponse.json({ areas: SAMPLE.areas }) }
